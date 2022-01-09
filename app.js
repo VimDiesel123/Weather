8ac0987338d427d59710c366767d70438cd66306
EddieSpaghetti666 <Dsmith141996@gmail.com>
2022-01-09 14:25:13 -0500

    Add skycons

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 window.addEventListener("load", () => {
   let long, lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6bd7ee9ffe2d64cf0fa1a487d4946163`;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=6bd7ee9ffe2d64cf0fa1a487d4946163`;
       fetch(api)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           console.log(data);
+          const { temp } = data.main;
+          const location = data.name;
+          const {description} = data.weather[0];
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = description;
+          locationTimezone.textContent = location;
+         
+          
         });
     });
   }
